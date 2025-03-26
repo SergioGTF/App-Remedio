fa72e62b93b70f017a396aea66bfcc53a3928ad6
SergioGTF <sergio23300033@aluno.cesupa.br>
2025-03-25 23:06:47 -0300

    feat: adicionar estilização aos botões
    - Adicionar estilização nos botões da tela principal com marginBottom
    
    Estrutura de pastas:
    
    - componentes/
       - Principal.js
       - AdicionarMedicamento.js
       - ListaMedicamentos.js
    - navegacao/
       - NavegacaoApp.js
    - App.js
    - index.js

--- App-Remedio/componentes/Principal.js
+++ App-Remedio/componentes/Principal.js
@@ -2,27 +2,39 @@ import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 
 const Home = ({ navigation }) => {
-return (
+    return (
         <View style={styles.container}>
-            <Text style={styles.text}>Bem-vindo ao App Remédio!</Text>
+        <Text style={styles.text}>Bem-vindo ao App Remédio!</Text>
+        <View style={styles.buttonContainer}>
+            <View style={styles.button}>
             <Button title="Adicionar Medicamento" onPress={() => navigation.navigate('AddMedicine')} />
+            </View>
+            <View style={styles.button}>
             <Button title="Listar Medicamentos" onPress={() => navigation.navigate('MedicineList')} />
+            </View>
+        </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-},
-text: {
-    fontSize: 20,
-    color: 'black',
-    marginBottom: 20,
-},
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+    text: {
+        fontSize: 20,
+        color: 'black',
+        marginBottom: 20,
+    },
+    buttonContainer: {
+        width: '80%',
+    },
+    button: {
+        marginBottom: 15,
+    },
 });
 
 export default Home;
